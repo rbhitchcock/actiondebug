479b697400e21be2e9b572efd9c9144f229207c0
Blake Hitchcock <rbhitchcock@gmail.com>
2014-04-01 17:17:06 -0400

    Move filter evaluation code to the Controller module

--- lib/actioncontroller_filter_chain/railtie.rb
+++ lib/actioncontroller_filter_chain/railtie.rb
@@ -2,7 +2,7 @@ module ActionControllerFilterChain
   class Railtie < Rails::Railtie
     initializer "controller_filter_chain.action_controller" do
       ActiveSupport.on_load(:action_controller) do
-        puts "extending #{self} with Wut::Controller"
+        puts "Extending #{self} with ActionControllerFilterChain"
         include ActionControllerFilterChain::Controller
       end
     end
