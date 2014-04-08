1c265783b66808e591a8825d30ddb5957ddc97c5
Blake Hitchcock <rbhitchcock@gmail.com>
2014-04-08 13:24:23 -0400

    Show filters for all controllers in the application

--- lib/actioncontroller_filter_chain/controller.rb
+++ lib/actioncontroller_filter_chain/controller.rb
@@ -21,6 +21,30 @@ module ActionControllerFilterChain
         filters kind: :after, action: action
       end
 
+      def show_filters_for_self_and_descendents(p = {})
+        list_descendants.reduce({self.name.to_sym => filters(p)}) do |h, descendant|
+          h[descendant.name.to_sym] = descendant.send(:filters, p)
+          h
+        end
+      end
+
+      def show_before_filters_for_self_and_descendents
+        show_filters_for_self_and_descendents({kind: :before})
+      end
+
+      def show_around_filters_for_self_and_descendents(p = {})
+        show_filters_for_self_and_descendents({kind: :around})
+      end
+
+      def show_after_filters_for_self_and_descendents
+        show_filters_for_self_and_descendents({kind: :after})
+      end
+
+      def list_descendants
+        Rails.application.eager_load! if Rails.env != "production"
+        descendants
+      end
+
       private
 
       def filter_for_kind?(filter, kind)
